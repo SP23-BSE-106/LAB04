f00608b2b642f09774c45f10437c4042f39c54b3
Munazza Javed <munazzajaved277@gmail.com>
2023-10-21 20:01:56 +0500

    Question 5 done of exercises.c

--- src/exercises.c
+++ src/exercises.c
@@ -96,6 +96,16 @@ int main(int argc, char **argv) {
   //5. Compute the summation of numbers 1 thru n and print
   //   the result; example: 1 + 2 + 3 + 4 + 5 = 15
 
+//Answer
+    int n, sum = 0;
+    printf("Enter a positive integer (n): ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++) {
+        sum += i;
+    }
+    printf("Sum of numbers 1 to %d = %d\n", n, sum);
+    
+
   //6. Compute the summation of squares of numbers 1 thru n
   //   and print the result; example: 1^2 + 2^2 + 3^2 + 4^2 + 5^2 = 55
 
