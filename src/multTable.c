85bfcc2b5c4711661c80d2d7330e13420f04a863
Munazza Javed <munazzajaved277@gmail.com>
2023-10-21 23:20:53 +0500

    Output added to multTable.c

--- src/multTable.c
+++ src/multTable.c
@@ -32,6 +32,19 @@ int main(int argc, char **argv)
 
 
 }
+/*
+OUTPUT:
 
-
-
+PS D:\LABWORK\CSCE155-C-Lab04\src> .\a.exe 6
+Multiplication Table for 6:
+6 x 1 = 6
+6 x 2 = 12
+6 x 3 = 18
+6 x 4 = 24
+6 x 5 = 30
+6 x 6 = 36
+6 x 7 = 42
+6 x 8 = 48
+6 x 9 = 54
+6 x 10 = 60
+*/
